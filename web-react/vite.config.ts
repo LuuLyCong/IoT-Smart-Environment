84f33174d47edfb77e99e4d5523261563974f87b
Kong-coder <luucong188205@gmail.com>
2026-09-20 16:22:14 +0000

    feat: setup vercel proxy to vps backend

--- web-react/vite.config.ts
+++ web-react/vite.config.ts
@@ -6,6 +6,12 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0',
-    port: 3000
+    port: 3000,
+    proxy: {
+      '/api/v1': {
+        target: 'http://localhost:8080',
+        changeOrigin: true
+      }
+    }
   }
 })
